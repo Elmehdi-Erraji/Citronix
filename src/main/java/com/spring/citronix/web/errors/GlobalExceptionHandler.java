66b911c7103d28010f851747f9340c55d29a84d9
Mehdi <elmehdiworkemail@gmail.com>
2024-11-17 14:46:39 +0000

    few changes on the entities

--- src/main/java/com/spring/citronix/web/errors/GlobalExceptionHandler.java
+++ src/main/java/com/spring/citronix/web/errors/GlobalExceptionHandler.java
@@ -36,7 +36,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleFileNotFoundException(FileNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
-    
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
