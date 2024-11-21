3f10ecdf23353fcbce659f02d5f38ca6d47b99d7
Mehdi <elmehdiworkemail@gmail.com>
2024-11-21 21:30:20 +0000

    tree ops refactored and tested

--- src/main/java/com/spring/citronix/domain/Tree.java
+++ src/main/java/com/spring/citronix/domain/Tree.java
@@ -30,9 +30,17 @@ public class Tree {
 
     public double calculateProductivity() {
         int age = calculateAge();
+        if (age > 20) return 0.0;
         if (age < 3) return 2.5;
         else if (age <= 10) return 12.0;
-        else if (age <= 20) return 20.0;
-        else return 0.0;
+        else return 20.0;
     }
+
+    public void validatePlantingDate() {
+        int plantingMonth = plantingDate.getMonthValue();
+        if (plantingMonth < 3 || plantingMonth > 5) {
+            throw new IllegalArgumentException("Trees can only be planted between March and May.");
+        }
+    }
+
 }
--- src/main/java/com/spring/citronix/repository/TreeRepository.java
+++ src/main/java/com/spring/citronix/repository/TreeRepository.java
@@ -1,6 +1,8 @@
 package com.spring.citronix.repository;
 
 import com.spring.citronix.domain.Tree;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,8 @@ import java.util.UUID;
 
 @Repository
 public interface TreeRepository extends JpaRepository<Tree, UUID> {
+    Page<Tree> findByFieldId(UUID fieldId, Pageable pageable);
+
     List<Tree> findByFieldId(UUID fieldId);
+
 }
