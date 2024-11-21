157471a0c00c51b210b30e570beac74913403476
Mehdi <elmehdiworkemail@gmail.com>
2024-11-21 23:30:05 +0000

    api documentation using swager added

--- src/main/java/com/spring/citronix/web/rest/FarmController.java
+++ src/main/java/com/spring/citronix/web/rest/FarmController.java
@@ -4,21 +4,26 @@ import com.spring.citronix.domain.Farm;
 import com.spring.citronix.domain.Field;
 import com.spring.citronix.service.FarmService;
 import com.spring.citronix.web.mapper.request.FarmMapper;
-
 import com.spring.citronix.web.vm.request.farm.FarmSearchVM;
 import com.spring.citronix.web.vm.request.farm.FarmUpdateVM;
-import jakarta.validation.Valid;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import org.springframework.data.domain.Page;
+
 import org.springframework.data.domain.Pageable;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/api/farm")
@@ -33,36 +38,62 @@ public class FarmController {
     }
 
     @PostMapping("/create")
+    @Operation(summary = "Save a new farm", description = "Creates a new farm and stores it in the database.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Farm created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public Farm createFarm(@RequestBody @Valid Farm farmDTO) {
-
         if (farmDTO.getFields() != null) {
             List<Field> fields = farmDTO.getFields().stream().map(listField -> {
                 Field field = new Field();
                 field.setArea(listField.getArea());
                 return field;
             }).collect(Collectors.toList());
-
             farmDTO.setFields(fields);
         }
-
         return farmService.save(farmDTO);
     }
 
     @GetMapping("/findById/{id}")
-    public ResponseEntity<Farm> findById(@PathVariable UUID id) {
+    @Operation(summary = "Find a farm by ID", description = "Retrieves a farm based on the provided farm ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Farm found successfully"),
+            @ApiResponse(responseCode = "404", description = "Farm not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<Farm> findById(
+
+            @PathVariable UUID id) {
         return farmService.findById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/findAll")
-    public Page<Farm> findAll(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+    @Operation(summary = "Get all farms", description = "Retrieves a list of all farms with pagination.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Farms retrieved successfully"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public Page<Farm> findAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
         Pageable pageable = PageRequest.of(page, size);
         return farmService.findAll(pageable);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteFarm(@PathVariable UUID id) {
+    @Operation(summary = "Delete a farm", description = "Deletes a farm by its ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Farm deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Farm not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<String> deleteFarm(
+
+            @PathVariable UUID id) {
         Optional<Farm> farm = farmService.findById(id);
         if (farm.isPresent()) {
             farmService.delete(farm.get());
@@ -72,12 +103,23 @@ public class FarmController {
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<Farm> updateFarm(@PathVariable UUID id, @RequestBody FarmUpdateVM farmUpdateVM) {
+    @Operation(summary = "Update a farm", description = "Updates farm details based on the provided ID and updated data.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Farm updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Farm not found"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<Farm> updateFarm(
+
+            @PathVariable UUID id,
+            @RequestBody FarmUpdateVM farmUpdateVM) {
         Optional<Farm> existingFarmOpt = farmService.findById(id);
 
         if (!existingFarmOpt.isPresent()) {
             return ResponseEntity.notFound().build();
         }
+
         Farm existingFarm = existingFarmOpt.get();
         Farm updatedFarm = farmMapper.toEntity(farmUpdateVM);
         updatedFarm.setId(id);
@@ -86,7 +128,14 @@ public class FarmController {
     }
 
     @PostMapping("/search")
-    public ResponseEntity<List<Farm>> searchFarm(@RequestBody @Valid FarmSearchVM farmSearch) {
+    @Operation(summary = "Search farms", description = "Search for farms by name, location, or date.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Farms retrieved successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid search parameters"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<List<Farm>> searchFarm(
+          @RequestBody @Valid FarmSearchVM farmSearch) {
         List<Farm> farms = farmService.searchFarms(
                 farmSearch.getName(),
                 farmSearch.getLocation(),
